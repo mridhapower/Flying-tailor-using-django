b4e0e64011088b6ed8ef77620b3bb226f2423830
Ashraful Mridha <amridha161170@bscse.uiu.ac.bd>
2021-05-08 16:11:09 +0600

    showing tailor list using table

--- tailor/tailors/views.py
+++ tailor/tailors/views.py
@@ -4,24 +4,25 @@ from .models import Location, Shop
 def location(request):
     #print(username=request)
     locations = Location.objects.all()
+    name=request.session['name']
     #loc=request.POST.get('loc')
     if (request.method == 'POST' and request.POST.get('loc')!=None):
         loc=request.POST.get('loc')
         print(loc)
         #id=Shop.objects.get(loc_id=loc)
         shops=Shop.objects.filter(loc_id=loc)
-        name=request.session['name']
+        
         print(name)
         #print(shops)
-        return render(request,'tailors.html',{'shops':shops,'loc':loc,'name':name})
+        return render(request,'tailors.html',{'shops':shops,'loc':loc,})
     else:
         button=request.POST.get('button')
         print(button)
-        return render(request,'locations.html',{'locations':locations})
+        return render(request,'locations.html',{'locations':locations,'name':name})
         #return redirect('location.html')
     
     
 
 def tailor(request):
-    return redirect('location.html')
+    return render(request,'locations.html')
     
